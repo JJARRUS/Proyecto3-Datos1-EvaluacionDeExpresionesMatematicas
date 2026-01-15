0bf5b142837406f0df2c14982b91b606b74f9366
JJARRUS <picadoj944@gmail.com>
2026-01-15 16:15:20 -0600

    Se creó el servidor TCP y se configuro la escucha para la comunicación cliente-servidor

--- src/Calculator.Server/CalculatorServer.cs
+++ src/Calculator.Server/CalculatorServer.cs
@@ -1,1 +1,22 @@
+using System.Net;
+using System.Net.Sockets;
 
+namespace Calculator.Server;
+
+public sealed class CalculatorServer
+{
+    private readonly int _port;
+
+    public CalculatorServer(int port)
+    {
+        _port = port;
+    }
+
+    public void Start()
+    {
+        var listener = new TcpListener(IPAddress.Any, _port);
+        listener.Start();
+
+        Console.WriteLine($"Servidor TCP escuchando en el puerto {_port}");
+    }
+}
--- src/Calculator.Server/Program.cs
+++ src/Calculator.Server/Program.cs
@@ -1,1 +1,9 @@
+using Calculator.Server;
 
+var port = 5000;
+
+var server = new CalculatorServer(port);
+server.Start();
+
+Console.WriteLine("Presione CTRL+C para cerrar el servidor");
+Console.ReadLine();
