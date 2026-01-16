d40e7a02fba2a982ddc9d0326eeb8df52e992707
JJARRUS <picadoj944@gmail.com>
2026-01-15 23:36:12 -0600

    Intento de merge, cambios minimos en los csproj

--- src/Calculator.Server/Program.cs
+++ src/Calculator.Server/Program.cs
@@ -1,1 +1,12 @@
+// US5: Punto de entrada del servidor TCP
+using Calculator.Server;
 
+// Puerto TCP en el que escuchará el servidor
+var port = 5000;
+
+// Crear e iniciar el servidor
+var server = new CalculatorServer(port);
+server.Start();
+
+Console.WriteLine("Presione CTRL+C para cerrar el servidor");
+Console.ReadLine();
